cea551c45929d5a2b35126573ece77817d3fbf6e
Stanisław Głogowski <stanislaw.glogowski@gmail.com>
2018-08-21 05:51:12 +0200

    Fixes imports

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 export * from "./types";
 export * from "./interfaces";
+export { ConnectorState } from "./constants";
 export * from "./errors";
 export * from "./utils";
 export { connectMetaMask as default } from "./utils";
--- src/utils.ts
+++ src/utils.ts
@@ -1,10 +1,12 @@
 import { IConnector, IConnectorOptions } from "./interfaces";
+import { TWindow } from "./types";
 import { Connector } from "./Connector";
 
 /**
  * connects MetaMask
  * @param options
+ * @param win
  */
-export function connectMetaMask(options: IConnectorOptions = {}): IConnector {
-  return Connector.create(options);
+export function connectMetaMask(options: IConnectorOptions = {}, win: TWindow = null): IConnector {
+  return Connector.create(options, win);
 }
